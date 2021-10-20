4d4b3ace7c2859f2c37af2ac1d1d6f16c52cc3e0
Andrew Martin <andy123@stanford.edu>
2021-10-20 15:22:31 -0700

    update

--- info.php
+++ info.php
@@ -2,6 +2,11 @@
 namespace Stanford\WhatsAppAlerts;
 /** @var WhatsAppAlerts $module */
 
+?>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.23/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/r-2.2.7/rg-1.1.2/sl-1.3.1/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.23/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/r-2.2.7/rg-1.1.2/sl-1.3.1/datatables.min.js"></script>
+<?php
+
 # Get all logs
 $fields = [
     "log_id",
@@ -36,8 +41,13 @@ print "<table id='logs'><thead><tr><th>" . implode("</th><th>", $fields) . "</th
 while ($row = db_fetch_assoc($q)) {
     print "<tr><td>" . implode("</td><td>",$row) . "</td></tr>";
 }
-print "</tbody></table>";
+print "</tbody></table>\n\n";
 ?>
 <script>
-    $('#logs').dataTable();
+    $(document).ready( function() {
+        // console.log('here');
+        $('#logs').DataTable({
+            select: true
+        });
+    });
 </script>
