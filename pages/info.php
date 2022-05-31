0c270d1e57c6f8503238c57284c24d32c63c9901
Andy Martin <andy123@stanford.edu>
2022-05-31 15:39:09 -0700

    TP-1629 moved info page to control center and updated config

--- pages/info.php
+++ pages/info.php
@@ -2,12 +2,50 @@
 namespace Stanford\WhatsAppAlerts;
 /** @var WhatsAppAlerts $module */
 
+/**
+ * This is a control panel info page that helps with configuration of Twilio for the instance
+ */
+
+
+loadJS('Libraries/clipboard.js');
 ?>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.23/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/r-2.2.7/rg-1.1.2/sl-1.3.1/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.23/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/r-2.2.7/rg-1.1.2/sl-1.3.1/datatables.min.js"></script>
+<script type="text/javascript">
+    // Copy-to-clipboard action
+    var clipboard = new Clipboard('.btn-clipboard');
+    // Copy the public survey URL to the user's clipboard
+    function copyUrlToClipboard(ob) {
+        // Create progress element that says "Copied!" when clicked
+        var rndm = Math.random()+"";
+        var copyid = 'clip'+rndm.replace('.','');
+        var clipSaveHtml = '<span class="clipboardSaveProgress" id="'+copyid+'">Copied!</span>';
+        $(ob).after(clipSaveHtml);
+        $('#'+copyid).toggle('fade','fast');
+        setTimeout(function(){
+            $('#'+copyid).toggle('fade','fast',function(){
+                $('#'+copyid).remove();
+            });
+        },2000);
+    }
+</script>
+<div class="card">
+    <div class="card-header">
+        <div class="card-title">Twilio Inbound Url</div>
+    </div>
+
+    <div class="card-body">
+        <p>The following url should be set in Twilio for inbound requests for all phone numbers associated with this project:</p>
+        <input id="inbound_url" value="<?php echo $module->getInboundUrl() ?>" onclick="this.select();" readonly="readonly" class="staticInput p-2" style="width:80%"/>
+        <button class="btn btn-defaultrc btn-clipboard" onclick="copyUrlToClipboard(this);" title="Copy to clipboard" data-clipboard-target="#inbound_url" style="padding:3px 8px 3px 6px;"><i class="fas fa-paste"></i></button>
+    </div>
+</div>
+
+
 <?php
 
-echo "<pre>Inbound Url:\n" . $module->getInboundUrl() . "</pre>";
+
+
 exit();
 
 
